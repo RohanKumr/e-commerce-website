85cad98396d8937a4ad4abd97fc30cfc720e26d1
Rohan Kumar <rkrohanrk065@gmail.com>
2023-02-03 22:52:49 +0530

    dots resize

--- components/simpleSlider/index.js
+++ components/simpleSlider/index.js
@@ -2,9 +2,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 import Slider from "react-slick";
+import { SliderWrapper } from "./styles";
 
 export function SimpleSlider() {
-  var settings = {
+  const settings = {
     dots: true,
     infinite: true,
     slidesToShow: 3,
@@ -12,65 +13,68 @@ export function SimpleSlider() {
     autoplay: true,
     speed: 3000,
     autoplaySpeed: 3000,
+    arrows: true,
     // cssEase: "linear",
   };
 
   return (
-    <Slider {...settings}>
-      <div>
-        <Image
-          src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          width="400"
-          height="400"
-          alt="anything"
-          priority
-        ></Image>
-      </div>
-      <div>
-        <Image
-          src="https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          width="400"
-          height="400"
-          alt="anything"
-          priority
-        ></Image>
-      </div>
-      <div>
-        <Image
-          src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          width="400"
-          height="400"
-          alt="anything"
-          priority
-        ></Image>
-      </div>
-      <div>
-        <Image
-          src="https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          width="400"
-          height="400"
-          alt="anything"
-          priority
-        ></Image>
-      </div>
-      <div>
-        <Image
-          src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          width="400"
-          height="400"
-          alt="anything"
-          priority
-        ></Image>
-      </div>
-      <div>
-        <Image
-          src="https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-          width="400"
-          height="400"
-          alt="anything"
-          priority
-        ></Image>
-      </div>
-    </Slider>
+    <SliderWrapper>
+      <Slider {...settings}>
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width="400"
+            height="400"
+            alt="anything"
+            priority
+          ></Image>
+        </div>
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width="400"
+            height="400"
+            alt="anything"
+            priority
+          ></Image>
+        </div>
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width="400"
+            height="400"
+            alt="anything"
+            priority
+          ></Image>
+        </div>
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width="400"
+            height="400"
+            alt="anything"
+            priority
+          ></Image>
+        </div>
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width="400"
+            height="400"
+            alt="anything"
+            priority
+          ></Image>
+        </div>
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            width="400"
+            height="400"
+            alt="anything"
+            priority
+          ></Image>
+        </div>
+      </Slider>
+    </SliderWrapper>
   );
 }
---
+++ components/simpleSlider/styles.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SliderWrapper = styled.div`
+  .slick-dots li button:before {
+    font-size: 12px;
+  }
+`;
